e3bf5e914562f845d270a64860e7858624bd5697
Erick Ruiz de Chavez <erick@erch.co>
2017-05-01 11:04:56 -0500

    DM script to greet users on channel join

---
+++ scripts/dm.js
@@ -0,0 +1,71 @@
+const { invert } = require('lodash');
+
+const monitored_channels = {
+  'compra-venta': 'Hola, bienvenidx a #compra-venta.\n\nPor favor, sigue la siguiente regla: *No hay reglas*.',
+};
+
+const users_white_list = [ 'eruizdechavez', 'mike', 'poguez', 'diegoaguilar', 'digaresc', 'josellau', 'ratacibernetica' ];
+
+function get_slack_channels(robot) {
+  return robot.adapter.client.web.channels.list().then(response => {
+    const channels = response.channels.reduce((hash, channel) => {
+      hash[channel.id] = channel.name;
+      return hash;
+    }, {});
+
+    return channels;
+  });
+}
+
+function monitor_channel_joins(robot, channels, monitored_channels) {
+  robot.adapter.client.on('raw_message', raw_message => {
+    const message = JSON.parse(raw_message);
+
+    // { name: text, ... } => [ name, name, ... ]
+    const monitored_channel_names = Object.keys(monitored_channels);
+
+    // { id: name, ... } => { name: id, ... }
+    const all_channels_with_id_by_name = invert(channels);
+
+    // [ name, name, ... ] => [ id, id, ... ]
+    const monitored_channel_ids = monitored_channel_names.map(name => all_channels_with_id_by_name[name]);
+
+    // If event is not a channel join or if it is not a monitored channel, then drop the event
+    if (message.type !== 'member_joined_channel' || monitored_channel_ids.indexOf(message.channel) === -1) {
+      return;
+    }
+
+    // console.log(raw_message);
+
+    // Only send the message if the user is in the white list
+    robot.adapter.client.web.users.info(message.user).then(response => {
+      if (users_white_list.indexOf(response.user.name) === -1) {
+        return;
+      }
+
+      send_private_message(robot, message.user, monitored_channels[channels[message.channel]]);
+    });
+  });
+}
+
+function send_private_message(robot, user, message) {
+  robot.adapter.client.web.im.open(user).then(
+    response => {
+      // console.log(response);
+      const options = {
+        as_user: true,
+        unfurl_links: true,
+        link_names: true,
+      };
+
+      robot.adapter.client.web.chat.postMessage(response.channel.id, message, options);
+    },
+    error => {
+      console.log(error);
+    }
+  );
+}
+
+module.exports = robot => {
+  get_slack_channels(robot).then(channels => monitor_channel_joins(robot, channels, monitored_channels));
+};
